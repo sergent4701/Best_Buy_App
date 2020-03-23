279c491c396f210d3b64d29820ec57cc73188d1f
Srdjan Bozin <srdjan.bozin@gmail.com>
2020-03-23 18:35:01 -0400

    Testing Databases

--- BestBuyUI/models.py
+++ BestBuyUI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    employeeId = models.CharField(max_length=20)
+    employeeType = models.CharField(max_length=20)
+    salt = models.TextField()
+    password = models.TextField()
+
+
+class Entry(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL)
