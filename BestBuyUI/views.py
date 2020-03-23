1c6838315eaf36504b7e2f191265f3437a755a98
Srdjan Bozin <srdjan.bozin@gmail.com>
2020-03-23 17:00:05 -0400

    More Stuff

---
+++ BestBuyUI/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'BestBuyUI/home.html')
+
+def about(request):
+    return HttpResponse('<h1>about</h1>')
