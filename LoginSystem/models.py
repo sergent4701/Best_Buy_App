a20d2fb13cc44cddaf7afd6bf3fd1c61ca2bb4a1
Srdjan Bozin <srdjan.bozin@gmail.com>
2020-03-25 15:38:43 -0400

    added more table stuff

--- LoginSystem/models.py
+++ LoginSystem/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.util import timezone
+from django.utils import timezone
 
 
 class Users(models.Model):
@@ -14,4 +14,20 @@ class Users(models.Model):
 
 class Entries(models.Model):
     revenue = models.DecimalField(max_digits=9, decimal_places=2)
+    revenuePerHour = models.DecimalField(max_digits=9, decimal_places=2)
+    bestBuyCards = models.IntegerField()
+    totalTechSupport = models.DecimalField(max_digits=9, decimal_places=2)
+    geekSquadSolutions = models.DecimalField(max_digits=9, decimal_places=2)
+    officeAttach = models.IntegerField()
     employee = models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
+    dateCreated = models.DateTimeField(default=timezone.now)
+
+
+class Goals(models.Model):
+    revenue = models.DecimalField(max_digits=9, decimal_places=2)
+    revenuePerHour = models.DecimalField(max_digits=9, decimal_places=2)
+    bestBuyCards = models.IntegerField()
+    totalTechSupport = models.DecimalField(max_digits=9, decimal_places=2)
+    geekSquadSolutions = models.DecimalField(max_digits=9, decimal_places=2)
+    officeAttach = models.IntegerField()
+    dateCreated = models.DateTimeField(default=timezone.now)
